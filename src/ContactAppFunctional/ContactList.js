a7a5efcfff6f244af469ae74a0f542b7e1d92b4e
ganesh2425 <saahoganesh023@gmail.com>
2021-03-25 16:39:16 +0530

    ok

--- src/ContactAppFunctional/ContactList.js
+++ src/ContactAppFunctional/ContactList.js
@@ -1,12 +1,12 @@
 import React from "react";
 let ContactList =(props)=>{
     return(<>
-    <pre>{JSON.stringify(props.contacts)}</pre>
+    <pre>{JSON.stringify(props.contact)}</pre>
     <div className="container">
         <div className="row">
             <div className="col">
                 <table className="table table-hover stripped">
-                    <thead className="table bg-info">
+                    <thead className="table bg-danger">
                         <tr>
                             <th>Name</th>
                             <th>Image</th>
@@ -15,26 +15,28 @@ let ContactList =(props)=>{
                             <th>Age</th>
                         </tr>
                     </thead>
-                    <tbody className="table-warning">
+                    <tbody className="table bg-warning">
                         {props.contacts.length > 0 ? (
                         <>
                         {props.contacts.map((contact)=>{
                             return(
-                                <tr key={contact.login.uuid}>
+                                <tr>
                                     <td>{contact.name.first}</td>
-                                    <td><img src={contact.picture.medium} alt="/" /></td>
+                                    <td><img src={contact.picture.medium}/></td>
                                     <td>{contact.email}</td>
                                     <td>{contact.location.city}</td>
                                     <td>{contact.dob.age}</td>
                                 </tr>
                             )
+
                         })}
-                        </>) : null}
+                        </>): null}
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
+    
     </>);
 }
 export default ContactList;
